9ea438d7a8a01440bb29d7633da0f542f0b71f9c
Higor Carniato <higorzen77@gmail.com>
2025-07-07 12:40:03 -0300

    feat: add LoginDto for authentication request

---
+++ DTOs/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementAPI.DTOs
+{
+    public class LoginDto
+    {
+
+        [Required(ErrorMessage = "O email é obrigatória")]
+        [EmailAddress(ErrorMessage = "Formato do email inválido.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        public string Password { get; set; }    
+    }
+}
